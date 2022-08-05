ba0ed289aed5a660ca80b12092e88cc84ebcbebf
everrr1025 <2037894@qq.com>
2022-08-05 14:14:46 +0800

    app.js as entry

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-import navigator from "./app/navigator/navigator.js";
-import content from "./app/content/content.js";
-import workspace from "./app/workspace/workspace.js";
-import { getUserData, getSubtree } from "./app/utils/chrome.js";
-import { setState1 } from "../src/state.js";
-import setting from "./app/setting/setting.js";
+import navigator from "./src/app/navigator/navigator.js";
+import content from "./src/app/content/content.js";
+import workspace from "./src/app/workspace/workspace.js";
+import { getUserData, getSubtree } from "./src/app/utils/chrome.js";
+import { setState1 } from "./src/state.js";
+import setting from "./src/app/setting/setting.js";
 function init() {
   getUserData(["easyBoard"]).then((userData) => {
     if (!(userData.easyBoard && userData.easyBoard.bookmarks.isSelected)) {
@@ -18,7 +18,7 @@ function init() {
 
       const isSelectedNodeId = userData.easyBoard.bookmarks.isSelected.id;
 
-      getSubtree(isSelectedNodeId).then((bkNodes) => {
+      getSubtree("1").then((bkNodes) => {
         setState1("workspace.primaryColor", primaryColor);
         setState1("bookmarks.bks", bkNodes[0]);
         setState1("bookmarks.path", [bkNodes[0]]);
